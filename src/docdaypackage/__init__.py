efa791aad8600ffc6095953c31170229f566bc11
Shiva Nadi <s.nadi@uu.nl>
2025-01-20 11:28:03 +0100

    first commit

---
+++ src/docdaypackage/__init__.py
@@ -0,0 +1,9 @@
+"""Documentation about docdaypackage."""
+
+import logging
+
+logging.getLogger(__name__).addHandler(logging.NullHandler())
+
+__author__ = "Shiva Nadi"
+__email__ = "s.nadi@uu.nl"
+__version__ = "0.1.0"
